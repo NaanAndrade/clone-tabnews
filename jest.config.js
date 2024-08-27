786c15543599d172af607d0eb70a2450607b1d53
naanandrade <naan.andrade2002@hotmail.com>
2024-08-26 21:25:36 -0300

    feat: adjusting jest.config.js and other files

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest();
+const jestConfig = createJestConfig();
+
+module.exports = jestConfig;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,4 @@
-import { database } from "../../../../../infra/database.js" // Todo: resolver erro: 'Cannot use import statement outside a module'
-
 test("GET to /api/v1/status returns 200", async () => {
-    const result = await database.query("SELECT 1 + 1;");
-    console.log(result)
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200)
 });
