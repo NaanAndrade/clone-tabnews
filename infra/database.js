f89e80e691e71c1c42fd10cbec1bce322ad2692c
naanandrade <naan.andrade2002@hotmail.com>
2024-08-26 21:28:49 -0300

    feat: adjust v1 status API

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,14 @@
 import { Client } from 'pg'; // Todo: resolver erro: 'Cannot use import statement outside a module'
 
 async function query(queryObject) {
-    const client = new Client();
+    console.log('queryObject: ', queryObject)
+    const client = new Client({
+        host: "localhost",
+        port: 5432,
+        user: "postgres",
+        database: "postgres",
+        password: "local_password",
+    });
     await client.connect();
     const result = await client.query(queryObject);
     await client.end();
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "200"})
+import { database } from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log('result: ', result.rows)
+  response.status(200).json({ chave: "valor"})
 }
 
 export default status
